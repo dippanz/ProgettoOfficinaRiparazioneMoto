dd76bd4fdf9619e6d0ddc08cc637245afaf57e2e
dippanz <emanuele110@tiscali.it>
2025-01-16 12:25:58 +0100

    Aggiunto statoriparazionedao

---
+++ BACKEND/src/main/java/it/officina/OfficinaRiparazioneMoto/controller/AccettazioneController.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package it.officina.OfficinaRiparazioneMoto.controller;
+
+/**
+ * 
+ */
+public class AccettazioneController {
+
+}
---
+++ BACKEND/src/main/java/it/officina/OfficinaRiparazioneMoto/dao/StatoRiparazioneDao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package it.officina.OfficinaRiparazioneMoto.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import it.officina.OfficinaRiparazioneMoto.model.StatoRiparazione;
+
+/**
+ * 
+ */
+public interface StatoRiparazioneDao extends JpaRepository<StatoRiparazione, Integer> {
+
+}
