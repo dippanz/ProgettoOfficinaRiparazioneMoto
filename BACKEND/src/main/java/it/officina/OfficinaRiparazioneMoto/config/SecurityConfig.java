052584bcdca9f38a00f49fd3eb6b5b2490427ef8
dippanz <94182988+dippanz@users.noreply.github.com>
2025-01-20 11:32:25 +0100

    Aggiunta classe di sicurezza (pattern spring security)

--- BACKEND/src/main/java/it/officina/OfficinaRiparazioneMoto/config/SecurityConfig.java
+++ BACKEND/src/main/java/it/officina/OfficinaRiparazioneMoto/config/SecurityConfig.java
@@ -5,10 +5,14 @@ package it.officina.OfficinaRiparazioneMoto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.DispatcherType;
 
 /**
  * 
@@ -17,28 +21,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
+	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    http
-	        .csrf(csrf -> csrf.disable())
+	        /*.csrf(csrf -> csrf.disable())
 	        .sessionManagement(session -> session
 	            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-	        )
+	        )*/
+	    	.securityMatcher("/api/**")
 	        .authorizeHttpRequests(authorize -> authorize
-	            //.requestMatchers("/login", "/resources/**", "/public/**").permitAll()
+	        	.dispatcherTypeMatchers(DispatcherType.FORWARD,DispatcherType.ERROR).permitAll()
+	            .requestMatchers("/","/resources/**", "/home/**").permitAll()
 	            //.requestMatchers("/admin/**").hasRole("ADMIN")
-	            .anyRequest().authenticated()
+	            .anyRequest().denyAll()
 	        )
-	        .formLogin(form -> form
+	        .formLogin(Customizer.withDefaults())
+	        /*.formLogin(form -> form
 	            .loginPage("/login")
-	            .defaultSuccessUrl("/home", true)
+	            .defaultSuccessUrl("/", true)
 	            .permitAll()
 	        )
 	        .logout(logout -> logout
 	            .permitAll()
-	        );
+	        )*/;
 
 	    return http.build();
 	}
+	
+	@Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
