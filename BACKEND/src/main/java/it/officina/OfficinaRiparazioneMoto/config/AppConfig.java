93e14739a22e298081410e1ed6ac9d823d929b69
dippanz <94182988+dippanz@users.noreply.github.com>
2025-01-25 13:14:19 +0100

    Aggiunta configurazione modelMapper

---
+++ BACKEND/src/main/java/it/officina/OfficinaRiparazioneMoto/config/AppConfig.java
@@ -0,0 +1,14 @@
+package it.officina.OfficinaRiparazioneMoto.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
